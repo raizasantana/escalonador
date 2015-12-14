e20d9accc1a613cff5ecbf94a1802cceda9396c5
Miguel Mendes <miguel.mdebrito@gmail.com>
2015-12-14 19:28:05 -0200

    adiciona fila inicio desenvolvimento

--- escalonador.c
+++ escalonador.c
@@ -10,6 +10,7 @@
 #define EXECUTANDO 2
 #define TEMINADO 3
 
+
 typedef struct _processo {
 	int id;
 	int id_proc_pai;
@@ -23,13 +24,15 @@ typedef struct _processo {
 typedef struct _fila {
 	Processos *fila[FILA_TAM];
 	int tam;
+	int tempo;
 } Fila;
 
 int esta_na_fila(Fila *fila, Processo *p)
 {
 	int i = 0;
 
-	for (int i = 0; i < fila->tam; i++) {
+	for (int i = 0; i < fila->tam; i++) 
+	{
 		if (fila->fila[i]->id == p->id)
 			return i;
 	}
@@ -42,9 +45,15 @@ int adiciona_na_fila(Fila *fila, Processo *p, Processo *pai)
 
 	int j;
 
-	if (pai->id )
+	if (esta_na_fila(fila, pai) == -1) 
+	{
+		fila->fila[fila->tam] = p;
+
+		p->status = EXECUTANDO;
+		p->tempo_estimado = p->tempo_estimado + fila->tempo;
+	}
 }
-void adiciona_na_fila(int *tam, int *fila, int p[M][7], int *id_processo, int *tempo_fila, int *tempo, int pos_pai)
+void adiciona_na_fila(int *tam, int *fila, int p[M][7], int *id_processo, int *tempo_fila, int *tempo_geral, int pos_pai)
 {
 
     *tam = *tam + 1;
@@ -80,7 +89,80 @@ void adiciona_na_fila(int *tam, int *fila, int p[M][7], int *id_processo, int *t
 
 
 }
+Fila *cria_fila_vazia()
+{
+	Fila *fila = malloc(sizeof(Fila));
+	fila->tam = 0;
+	fila->tempo = 0;
+
+	return fila;
+}
+void le_processos(Processos processos[])
+{
+	int n = 0, i;
+	FILE *arq;
+	arq = fopen("processos.txt","r");
+
+	if(arq == NULL)
+	{
+		printf("Erro ao ler arquivo.");
+		return;
+	}
+
+	fscanf(arq,"%d", &n);
+
+	for (i = 0; i < n; i++)
+	{
+		fscanf(arq,"%d %d %d %d %d %d", &processos[i]->id, 
+										&processos[i]->id_proc_pai, 
+										&processos[i]->tempo_estimado,
+										&processos[i]->tempo_real, 
+										&processos[i]->status, 
+										&processos[i]->ut_de_entrada);
+		
+	}
+
+	fclose(arq);
+
+}
 int main() 
 {
+	FILE *arq_resultado;
 
+	Processos processos[M];
+
+	Fila *fila1 = cria_fila_vazia(),
+		 *fila2 = cria_fila_vazia();
+
+
+	le_processos(processos);
+
+
+	int i, f1 = 0, f2 = 0, j, k;
+	int tempo_geral = 0, tempo_f1 = 0, tempo_f2 = 0;
+	int tr_f1 = 1, tr_f2 = 0;
+
+	zeros(fila1, FILA_TAM);  
+	zeros(fila2, FILA_TAM); 
+	// memset(fila1, 0, FILA_TAM * sizeof(int));
+	// memset(fila2, 0, FILA_TAM * sizeof(int));
+
+
+	le_processos(p);
+
+	//Primeiro Processo entra na fila 1
+	fila1[0] = p[0][ID];
+	f1++;
+
+	//Adicionando o tempo do processo na fila
+	tempo_geral = tempo_f1 + p[0][TEMPO_ESTIMADO];
+	p[0][TEMPO_ESTIMADO] = tempo_geral;
+	tempo_f1 = tempo_geral;
+
+	for(j = 0; j< 20; j++) //Processos iniciando ciclos = 0
+	p[j][CICLOS] = 0;
+	i = 1;
+
+	//Escalonamento
+	int ciclos = 1;
 }
